5822832eb0fb0031c0f44bb4890e2a93293c0d4c
JojiKoike <joji.koike@gmail.com>
2018-11-06 12:47:55 +0900

    Lake Counting DFS Sample

--- Basic/lakecounting/lakecounting.go
+++ Basic/lakecounting/lakecounting.go
@@ -10,10 +10,13 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	// Number of Rows
 	scanner.Scan()
 	N, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	// Number of Columns
 	scanner.Scan()
 	M, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	// Reading Field Info
 	field := [][]string{}
 	for i := 0; i < N; i++ {
 		scanner.Scan()
@@ -21,10 +24,29 @@ func main() {
 		field = append(field, row)
 	}
 
+	var res = 0
 	for i := 0; i < N; i++ {
 		for j := 0; j < M; j++ {
-			fmt.Print(field[i][j] + "\t")
+			if field[i][j] == "W" {
+				field = dfs(i, j, N, M, field)
+				res++
+			}
 		}
-		fmt.Print("\n")
 	}
+	fmt.Printf("Num of Lakes : %d\n", res)
+}
+
+func dfs(x int, y int, N int, M int, field [][]string) [][]string {
+	field[x][y] = "."
+
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			var nx = x + dx
+			var ny = y + dy
+			if 0 <= nx && nx < N && 0 <= ny && ny < M && field[nx][ny] == "W" {
+				field = dfs(nx, ny, N, M, field)
+			}
+		}
+	}
+	return field
 }
